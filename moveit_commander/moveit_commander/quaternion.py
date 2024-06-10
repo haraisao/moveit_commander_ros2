c3d71b0ea19bd46682101981a479f62fb15b3210
Isao Hara <haraisao@gmail.com>
2024-06-10 13:17:48 +0900

    bugfixed

--- moveit_commander/moveit_commander/quaternion.py
+++ moveit_commander/moveit_commander/quaternion.py
@@ -20,17 +20,17 @@ def euler_to_quaternion(roll, pitch, yaw):
 def quaternion_to_euler(qx, qy, qz, qw):
     sinr_cosp = 2.0 * (qw * qx + qy * qz)
     cosr_cosp = 1.0 - 2.0 * (qx * qx + qy * qy)
-    roll = np.atan2(sinr_cosp, cosr_cosp)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
 
     sinp = 2.0 * (qw * qy - qz * qx)
     if np.fabs(sinp) >= 1:
         pitch = np.copsign(np.pi / 2, sinp)
     else:
-        pitch = np.asin(sinp)
+        pitch = np.arcsin(sinp)
 
     siny_cosp = 2.0 * (qw * qz + qx * qy)
     cosy_cosp = 1.0 - 2.0 * (qy * qy + qz * qz)
-    yaw = np.atan2(siny_cosp, cosy_cosp)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
 
     return [roll, pitch, yaw]
 
